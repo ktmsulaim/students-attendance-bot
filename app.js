4037a119ed3fc23ad42ab611723ffefb520d89f6
Ktm Sulaim <ktmsulaim@Ktms-iMac.local>
2022-01-22 23:10:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,325 @@
+import { Telegraf, Markup } from 'telegraf';
+import db from './db.js'
+import moment from 'moment'
+import cron from 'node-cron'
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start(ctx => {
+    const username = ctx.message.from.username;
+    const chatType = ctx.message.chat.type;
+
+    if (chatType == 'private') {
+        ctx.reply(`Hello ${username}, thank you for using me!`)
+    } else if (chatType == 'group') {
+        const groupName = ctx.message.chat.title;
+        ctx.reply(`Hello ${username}, welcome to ${groupName} group!`)
+    }
+});
+
+bot.hears(['Assalamu Alaikum', 'assalamu alaikum', 'Assalamu alaikum'], ctx => {
+    ctx.reply('Wa alaikumussalam');
+})
+
+bot.command('register_admin', ctx => {
+    // Only proceed if the command is from a group
+    // Check the sender is an admin
+
+})
+
+bot.command('attendance_today', async ctx => {
+    if (ctx.message.chat.type != 'group') {
+        return;
+    }
+
+    const group_id = ctx.message.chat.id;
+    const date = moment.unix(ctx.message.date).format('YYYY-MM-DD');
+    const present = await db.getAttendanceOfTheDay({ group_id, date, type: 'present' })
+    console.log('Present: \n', present);
+
+    let message;
+    let count = 1;
+
+    if (present && present.length) {
+        message = `<b>Present today:</b> \n\n`;
+        present.forEach((atd) => {
+            message += `${count}. ${atd.name} - ${moment(atd.created_at).format('hh:mm:ss a')} \n`;
+
+            count++;
+        })
+    }
+
+    const absent = await db.getAttendanceOfTheDay({ group_id, date, type: 'absent' })
+    console.log('Absent: \n', absent);
+    count = 1;
+
+    if (absent && absent.length) {
+        message += `\n\n<b>Absent today:</b> \n\n`;
+        let count = 1;
+        absent.forEach((atd) => {
+            message += `${count}. ${atd.name} \n`;
+
+            count++;
+        })
+    }
+
+    const notMarkedAttendance = await db.getNotMarkedAttendanceOfTheDay({ date, group_id })
+    count = 1;
+    console.log('Not marked: \n', notMarkedAttendance);
+
+    if (notMarkedAttendance && notMarkedAttendance.length) {
+        message += `\n\n<b>Not marked attendance:</b>\n\n`;
+
+        notMarkedAttendance.forEach(ntd => {
+            message += `${count}. ${ntd.name} \n`;
+
+            count++;
+        })
+    }
+
+    ctx.replyWithHTML(message);
+})
+
+bot.command('test', ctx => {
+    // ctx.replyWithHTML("<b>OhH! You were testing!</b>", Markup.inlineKeyboard([ Markup.button.callback('Present', '/in'), Markup.button.callback('Absent', '/out') ]));
+    return ctx.reply('Oh You are testing with inline keyboard', Markup.inlineKeyboard([Markup.button.callback('Present', 'present'), Markup.button.callback('Absent', 'absent')]).oneTime().resize())
+})
+
+bot.action('present', ctx => {
+    attendance(ctx, true)
+})
+bot.action('absent', ctx => {
+    attendance(ctx, true)
+})
+
+bot.command('register_group', async ctx => {
+    const group_id = ctx.message.chat.id;
+    const group_name = ctx.message.chat.title;
+
+    const registered = await db.registerGroup({ group_id, group_name })
+
+    if (registered == 'group registration failed') {
+        ctx.reply('The group was already registered!');
+    }
+
+    if (registered == true) {
+        ctx.reply(`The group ${group_name} was successfully registered`);
+    }
+
+    if (registered == false) {
+        ctx.reply('Unable to register the group!');
+    }
+})
+
+
+
+bot.command(['in', 'out'], attendance)
+
+async function attendance(context, callback = false) {
+    let ctx = {};
+    Object.assign(ctx, context);
+
+    if (callback) {
+        console.log(ctx.update.callback_query);
+        ctx.message = ctx.update.callback_query.message;
+    }
+    // check the sender alreay in the students table
+    // insert one if not exists
+    // add attendance for today if not done today
+    const chatType = ctx.message.chat.type;
+
+    console.log("Chat type should be group:", chatType);
+
+    if (chatType != 'group') {
+        return;
+    }
+
+    /**
+     * Attendance should be between 6:30 - 9:00 
+     */
+
+    const telegram_id = ctx.message.from.id;
+    const group_id = ctx.message.chat.id;
+    const name = ctx.message.from.username;
+    const group_name = ctx.message.chat.title
+    const message_time = moment.unix(ctx.message.date)
+    const start_time = moment().toDate().setHours(process.env.START_HOUR, process.env.START_MINUTE, 0)
+    const end_time = moment().toDate().setHours(process.env.END_HOUR, process.env.END_MINUTE, 0)
+
+    // if its between 6:30 & 9:00
+    if (!message_time.isAfter(start_time) || !message_time.isBefore(end_time)) {
+        context.reply('Hmm! attendance facility is not available now. Please try again between 6:30 am and 9:00 am')
+        return;
+    }
+
+    // check is holiday or any holi dates
+    const holiday = process.env.HOLI_DAY
+    const today = moment()
+    let holidates = process.env.HOLI_DATES;
+    holidates = holidates ? JSON.parse(holidates) : []
+
+    if (today.format('dddd') == holiday || holidates.includes(today.format('DD-MM-YYYY'))) {
+        context.reply('Ooh! Are you going to mark the attendance on holiday?');
+        return;
+    }
+
+
+    console.log("Checking whether a student is registered with telegram id and group id");
+    const studentExists = await db.isStudentExist(telegram_id, group_id);
+    console.log("The student is :", studentExists);
+
+    if (!studentExists) {
+        const studentData = {
+            group_id,
+            telegram_id,
+            name,
+            group_name,
+        }
+
+        console.log(`A new student is being created: ${name}, Telegram ID: ${telegram_id}, Group: ${group_name} (${group_id})`);
+
+        await db.createStudent(studentData);
+        console.log("A new student was created!");
+    }
+
+    const command = ctx.message.text;
+    const data = {
+        telegram_id,
+        group_id,
+        type: command == '/in' ? 'present' : 'leave',
+        date: moment.unix(ctx.message.date).format('YYYY-MM-DD')
+    }
+
+    if (command == '/out') {
+        context.telegram.sendMessage(ctx.message.from.id, 'Hey! are you on leave today?')
+        console.log(`${name} is leave on today`);
+    }
+
+    console.log("Checking the student has attended today");
+
+    const attendanceMarked = await db.markAttendance(data)
+
+    if (!attendanceMarked) {
+        console.log("Unable to mark the attendance");
+        context.reply(`Sorry ${name}, there was an error while marking your attendance!`)
+    }
+
+    if (attendanceMarked == true) {
+        console.log("The attendance was marked");
+        const time = moment.unix(ctx.message.date).format('hh:mm:ss a')
+        context.reply(`${name} has marked attendance on ${time}`)
+    }
+
+    if (attendanceMarked == 'already attended') {
+        const attendance = await db.getAttendance(data)
+
+        if ((command == '/out' && attendance.type == 'present') || (command == '/in' && attendance.type == 'absent')) {
+            data.date = moment.unix(ctx.message.date).format('YYYY-MM-DD HH:mm:ss');
+            const time = moment.unix(ctx.message.date).format('hh:mm:ss a')
+            await db.updateAttendance(data);
+            context.reply(`${name} has updated attendance on ${time}`)
+        } else {
+            context.reply(`${name} has already marked attendance on ${moment(attendance.updated_at ? attendance.updated_at : attendance.created_at).format('hh:mm:ss A')}`)
+        }
+
+    }
+}
+
+async function sheduleAttendence() {
+    console.log("The scheduler has started");
+
+    console.log("checking for registered groups");
+
+    const groups = await db.getRegisteredGroups()
+    console.log(groups);
+
+    cron.schedule('* 30 6 * * *', () => {
+        // check is holiday or any holi dates
+        const holiday = process.env.HOLI_DAY
+        const today = moment()
+        let holidates = process.env.HOLI_DATES;
+        holidates = holidates ? JSON.parse(holidates) : []
+
+        if (today.format('dddd') == holiday || holidates.includes(today.format('DD-MM-YYYY'))) {
+            return;
+        }
+        if (groups && groups.length) {
+            console.log(`Total groups ${groups.length}`);
+            groups.forEach(group => {
+                const date = moment().format('DD-MM-YYYY')
+                let message = `<b>${date}</b>\n`;
+                message += `അസ്സലാമു അലൈകും,\nപ്രിയപ്പെട്ട വിദ്യാർത്ഥികളെ, സുഖം തന്നെയല്ലേ?`
+                message += `\n<b>ഹാജർ പറയൂ</b>`
+                bot.telegram.sendMessage(group.group_id, message, {
+                    parse_mode: 'HTML',
+                    ...Markup.inlineKeyboard([
+                        Markup.button.callback('Present', 'present'),
+                        Markup.button.callback('Absent', 'absent')
+                    ])
+                })
+            })
+            console.log("Messages were sent to each groups");
+        }
+    })
+
+    cron.schedule('* 0 9 * * *', () => {
+        if(groups && groups.length) {
+            groups.forEach(async group => {
+                const group_id = group.group_id;
+                const date = moment().format('YYYY-MM-DD');
+                const present = await db.getAttendanceOfTheDay({ group_id, date, type: 'present' })
+                console.log('Present: \n', present);
+        
+                let message;
+                let count = 1;
+        
+                if (present && present.length) {
+                    message = `<b>Present today:</b> \n\n`;
+                    present.forEach((atd) => {
+                        message += `${count}. ${atd.name} - ${moment(atd.created_at).format('hh:mm:ss a')} \n`;
+        
+                        count++;
+                    })
+                }
+        
+                const absent = await db.getAttendanceOfTheDay({ group_id, date, type: 'absent' })
+                console.log('Absent: \n', absent);
+                count = 1;
+        
+                if (absent && absent.length) {
+                    message += `\n\n<b>Absent today:</b> \n\n`;
+                    let count = 1;
+                    absent.forEach((atd) => {
+                        message += `${count}. ${atd.name} \n`;
+        
+                        count++;
+                    })
+                }
+        
+                const notMarkedAttendance = await db.getNotMarkedAttendanceOfTheDay({ date, group_id })
+                count = 1;
+                console.log('Not marked: \n', notMarkedAttendance);
+        
+                if (notMarkedAttendance && notMarkedAttendance.length) {
+                    message += `\n\n<b>Not marked attendance:</b>\n\n`;
+        
+                    notMarkedAttendance.forEach(ntd => {
+                        message += `${count}. ${ntd.name} \n`;
+        
+                        count++;
+                    })
+                }
+        
+                bot.telegram.sendMessage(group_id, message, {parse_mode: 'HTML'});
+            })
+        }
+    })
+}
+
+sheduleAttendence()
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
---
+++ db.js
@@ -0,0 +1,147 @@
+import 'dotenv/config';
+
+import mysql from 'mysql2/promise'
+
+var connection = await mysql.createConnection({
+    host     : process.env.MYSQL_HOST,
+    user     : process.env.MYSQL_USERNAME,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+  });
+ 
+  /**
+   * Create tables for storing data
+   * 
+   * 1. Users
+   * 2. Students
+   * 3. Attendance
+   */
+  await connection.query("CREATE TABLE IF NOT EXISTS `users` (`id` int AUTO_INCREMENT,`telegram_id` bigint,`group_id` varchar(255),`group_name` varchar(255),`name` varchar(255), PRIMARY KEY (id))");
+  await connection.query("CREATE TABLE IF NOT EXISTS `students` (`id` bigint AUTO_INCREMENT,`telegram_id` bigint,`group_id` varchar(255),`group_name` varchar(255),`name` varchar(255), PRIMARY KEY (id))");
+  await connection.query("CREATE TABLE IF NOT EXISTS `attendance` (`id` int AUTO_INCREMENT,`student_id` bigint,`type` varchar(255) DEFAULT 'present',`remarks` varchar(255),`created_at` datetime DEFAULT NOW(),`updated_at` datetime, PRIMARY KEY (id), FOREIGN KEY (`student_id`) REFERENCES `student_attendance_bot`.`students` (`id`) ON DELETE CASCADE)")
+  await connection.query("CREATE TABLE IF NOT EXISTS `groups` (`id` int AUTO_INCREMENT,`group_id` bigint,`group_name` varchar(255), PRIMARY KEY (id))");
+
+  export default {
+      isStudentExist: async function(telegram_id, group_id) {
+          const [rows, fields] = await connection.query({ sql: `SELECT * FROM students WHERE telegram_id = ? AND group_id = ?`, values: [telegram_id, group_id]})
+
+          return rows.length;
+      },
+      getStudent: async function(telegram_id, group_id) {
+        const [rows, fields] = await connection.query({ sql: `SELECT * FROM students WHERE telegram_id = ${telegram_id} AND group_id = ${group_id}`})
+
+        return rows[0];
+      },
+      createStudent: async function(data) {
+        const [inserted] = await connection.query("INSERT INTO `students` SET ?", {telegram_id: data.telegram_id, group_id: data.group_id, group_name: data.group_name, name: data.name});
+        console.log(inserted);
+        return inserted;
+      },
+      hasAttended: async function(data) {
+        const telegram_id = data.telegram_id;
+        const group_id = data.group_id;
+
+        const studentExists = await this.isStudentExist(telegram_id, group_id);
+
+        if(studentExists) {
+            const student = await this.getStudent(telegram_id, group_id)
+
+            if(student != null || student != undefined) {
+                console.log(`Check ${student.name} has marked attendance on ${data.date}`);
+                const [attendance] = await connection.query(`SELECT * FROM attendance WHERE student_id = ${student.id} AND CAST(created_at AS DATE) = ?`, [data.date])
+    
+                return attendance.length;
+            }
+        }
+
+        return false;
+      },
+      markAttendance: async function(data) {
+        const hasAttended = await this.hasAttended(data);
+
+        const telegram_id = data.telegram_id;
+        const group_id = data.group_id;
+
+        const studentExists = await this.isStudentExist(telegram_id, group_id);
+
+        if(studentExists) {
+            if(!hasAttended) {
+                const student = await this.getStudent(telegram_id, group_id)
+
+                if(student != null || student != undefined) {
+                    const attend = await connection.query("INSERT INTO `attendance` SET ?", {student_id: student.id, type: data.type})
+    
+                    return true;
+                }
+            }
+
+            return "already attended";
+        }
+
+        return false;
+
+      },
+      updateAttendance: async function(data) {
+        const telegram_id = data.telegram_id;
+        const group_id = data.group_id;
+
+        const studentExists = await this.isStudentExist(telegram_id, group_id);
+
+        const hasAttended = await this.hasAttended(data);
+
+        if(studentExists && hasAttended) {
+            const student = await this.getStudent(telegram_id, group_id)
+            const update = await connection.query("UPDATE attendance SET ?", {student_id: student.id, type: data.type, updated_at: data.date})
+
+            return true;
+        }
+
+        return false;
+      },
+      getAttendance: async function(data) {
+        const student = await this.getStudent(data.telegram_id, data.group_id)
+
+        if(student != null || student != undefined) {
+            const [attend] = await connection.query(`SELECT * FROM attendance WHERE student_id = ${student.id} AND CAST(created_at AS DATE) = ?`, [data.date])
+
+            return attend && attend.length ? attend[0] : [];
+        }
+      },
+      getAttendanceOfTheDay: async function(data) {
+          const date = data.date;
+          const type = data.type;
+          const [rows] = await connection.query(`SELECT attendance.created_at, attendance.type, students.name FROM attendance RIGHT JOIN students ON students.id = attendance.student_id WHERE students.group_id = ? AND CAST(attendance.created_at AS DATE) = ? AND attendance.type = ? ORDER BY attendance.created_at`, [data.group_id, date, type])
+
+          return rows;
+      },
+      getNotMarkedAttendanceOfTheDay: async function(data) {
+          const [rows] = await connection.query('SELECT students.name, students.id FROM students WHERE students.id NOT IN (SELECT attendance.student_id from attendance WHERE CAST(attendance.created_at AS DATE) = ?) AND students.group_id = ?', [data.date, data.group_id])
+
+          return rows;
+      },
+      isGroupRegistered: async function(group_id) {
+          const [rows] = await connection.query("SELECT * FROM `groups` WHERE group_id = ?", [group_id]);
+
+          return rows && rows.length;
+      },
+      registerGroup: async function(data) {
+        const isRegistered = await this.isGroupRegistered(data.group_id);
+
+        if(isRegistered) {
+            return "group registration failed";
+        }
+
+        if(!isRegistered) {
+            await connection.query("INSERT INTO `groups` SET ?", {group_id: data.group_id, group_name: data.group_name})
+            return true;
+        }
+
+        return false;
+
+      },
+      getRegisteredGroups: async function() {
+        const [groups] = await connection.query("SELECT * FROM `groups`");
+
+        return groups;
+      }
+  };
